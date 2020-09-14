03ae1d6caec6768d81865edc464d283e6de7d84b
leolecrado <leocrapart@yahoo.fr>
2020-09-14 22:25:15 +0200

    new message

--- api/api/index.js
+++ api/api/index.js
@@ -1,5 +1,6 @@
 export default (req, res) => {
 	res.json({
-		message: "Les fleurs sont roses et les jardins enchantés."
+		message: "Les fleurs sont roses et les jardins enchantés.",
+		message2: "Et nous découvrons la beauté de ces jardins."
 	})
 }
